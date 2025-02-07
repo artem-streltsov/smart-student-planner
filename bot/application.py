e579abe38c3537b1c66b22c68f17e41b233285fb
sauceboss44 <alexrudakov5@gmail.com>
2025-02-07 12:44:46 +0000

    add events command handler

--- bot/application.py
+++ bot/application.py
@@ -11,7 +11,7 @@ from bot.commands.start import start
 from bot.commands.help import help
 from bot.commands.set_ics import set_ics
 from bot.commands.today import today
-
+from bot.commands.events import events
 from bot.config import config
 from bot.handlers.misc import unknown, echo
 
@@ -26,5 +26,5 @@ application.add_handler(CommandHandler("start", start))
 application.add_handler(CommandHandler("help", help))
 application.add_handler(CommandHandler("setics", set_ics))
 application.add_handler(CommandHandler("today", today))
-
+application.add_handler(CommandHandler("events", events))
 application.add_handler(MessageHandler(filters.TEXT & (~filters.COMMAND), echo))
